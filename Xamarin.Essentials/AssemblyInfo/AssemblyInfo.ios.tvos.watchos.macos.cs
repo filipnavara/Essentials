f730817c532ba59d652afedcbb87093af5f0a4c5
Filip Navara <filip.navara@gmail.com>
2023-07-21 21:59:46 +0200

    Another attempt at making the assembly trimmable

--- Xamarin.Essentials/AssemblyInfo/AssemblyInfo.ios.tvos.watchos.macos.cs
+++ Xamarin.Essentials/AssemblyInfo/AssemblyInfo.ios.tvos.watchos.macos.cs
@@ -3,3 +3,4 @@
 #pragma warning disable CS0618 // Type or member is obsolete
 [assembly: LinkerSafe]
 #pragma warning restore CS0618 // Type or member is obsolete
+[assembly: AssemblyMetadata("IsTrimmable", "True")]
